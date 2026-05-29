3479b998d6a449e3567da7b2ee455f27af7e2922
moayed-abdalla <moayed.h.abdalla@gmail.com>
2026-05-29 17:28:05 +0300

    File clean up 2

--- db/default_temperature_unit.sql
+++
@@ -1,4 +0,0 @@
--- Default temperature unit preference for recipe step conversions (C = Celsius, F = Fahrenheit)
-ALTER TABLE profiles
-  ADD COLUMN IF NOT EXISTS default_temperature_unit TEXT NOT NULL DEFAULT 'C'
-  CHECK (default_temperature_unit IN ('C', 'F'));
